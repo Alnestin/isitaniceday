09e39b50d59b761bc0659fd17338b60094b887ef
Martin Heberling <martin.heberling.dalies@gmail.com>
2022-09-10 23:24:01 -0400

    Can now send information to firebase database.

--- src/LikeButtons.js
+++ src/LikeButtons.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { Button, Container } from "@mui/material";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebase.js";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+// const analytics = getAnalytics(app);
 
 export default function LikeButtons(props) {
   if (!props.temp) {
@@ -7,9 +16,27 @@ export default function LikeButtons(props) {
   }
 
   function sendInfo(data) {
-    // TODO: Send data to database and add counter to app.
-    console.log(data)
+    let value = 0;
+    const dbref = ref(db, data + '/');
+    if (data === "Like") {
+      // Get current value
+      onValue(dbref, (snapshot) => {
+        value = parseInt(snapshot.val()) + 1;
+      });
+
+      // Update value in database.
+      set(dbref, value);
+    } else {
+      // Get current value
+      onValue(dbref, (snapshot) => {
+        value = parseInt(snapshot.val()) + 1;
+      });
+
+      // Update value in database
+      set(dbref, value);
+    }
   }
+
   return (
     <Container>
         <Button variant='contained' onClick={() => sendInfo('Like')}>Like</Button>
